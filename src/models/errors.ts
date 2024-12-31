a53986f25c3bfe68b8617effe02b54d1a8d9a897
avivilloz <avivilloz@gmail.com>
2024-12-31 18:28:50 +0200

    updating repository

--- src/models/errors.ts
+++
@@ -1,9 +0,0 @@
-export class ScriptExecutionError extends Error {
-    constructor(
-        message: string,
-        public readonly originalError?: Error
-    ) {
-        super(message);
-        this.name = 'ScriptExecutionError';
-    }
-}
